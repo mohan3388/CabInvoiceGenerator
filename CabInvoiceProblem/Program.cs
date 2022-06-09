ee3d5cb94306bc657716ac4b240bae4d0eeb3109
mohan338 <sahumohan338@gmail.com>
2022-06-09 14:05:29 +0530

    [mohan]add: calcute fair given distance and time

--- CabInvoiceProblem/Program.cs
+++ CabInvoiceProblem/Program.cs
@@ -5,8 +5,30 @@ public class Program
 {
     public static void Main(String[]args)
     {
-        CabInvoiceGenerator problem = new CabInvoiceGenerator(RideType.NORMAL);
-        double Result = problem.calculateFair(15, 5);
-        Console.WriteLine(Result);
+        Console.WriteLine("welcome to the program");
+        bool check = true;
+        while (check)
+        {
+            Console.WriteLine("1.Display Fair Of Cab\n2. Display Fair Cab Of Multiple Rides");
+            Console.WriteLine("Enter the Above Option");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    CabInvoiceGenerator generator = new CabInvoiceGenerator(RideType.NORMAL);
+                    double result = generator.calculateFair(15, 5);
+                    Console.WriteLine("fair of cab" + result);
+                    break;
+               
+                case 2:
+                    CabInvoiceGenerator invoice = new CabInvoiceGenerator(RideType.PREMIER);
+                    Ride[] rides = { new Ride(15, 5), new Ride(25, 10), new Ride(20, 20) };
+                    double result1 = invoice.CalculateMultipleRide(rides);
+                    Console.WriteLine("fair of cab multiple rides" + result1);
+                    break;
+                default: Console.WriteLine("enter correct option"); 
+                    break;
+            }
+        }
     }
 }
--- CabInvoiceTest/UnitTest1.cs
+++ CabInvoiceTest/UnitTest1.cs
@@ -13,5 +13,13 @@ namespace CabInvoiceTest
             double result = invoice.calculateFair(15, 5);
             Assert.AreEqual(result, 250);
         }
+        [Test]
+        public void InputInInteger_ShouldReturn_MultipleRides_TotalFair()
+        {
+            CabInvoiceGenerator invoice = new CabInvoiceGenerator(RideType.PREMIER);
+            Ride[] rides = { new Ride(2, 3), new Ride(4, 5), new Ride(5, 6) };
+            double result = invoice.CalculateMultipleRide(rides);
+            Assert.AreEqual(result, 46);
+        }
     }
 }
